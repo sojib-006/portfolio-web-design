546cd46203d9314579c265504acd385cb3c403c9
sojib-006 <abir.bd006@gmail.com>
2022-09-13 17:16:32 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(".service_slider").slick({
+    arrows: true,
+    prevArrow: '<i class="fa-solid fa-arrow-right-lon prev"></i>',
+    nextArrow: '<i class="fa-solid fa-arrow-left-lon next"></i>',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+})
+$(".testi_sliter").slick({
+    arrows: true,
+    prevArrow: '<i class="fa-solid fa-arrow-right-lon prev"></i>',
+    nextArrow: '<i class="fa-solid fa-arrow-left-lon next"></i>',
+    slidesToShow: 2,
+    slidesToScroll: 1,
+})
+var mixer = mixitup('.pro-images');
